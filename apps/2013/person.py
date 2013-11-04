aefed679652c8c65b478e7aacdd89bc58c83c783
Zoom.Quiet <zoomquiet+git@gmail.com>
2013-11-04 19:27:43 +0800

    init. PyCon2013China site

---
+++ apps/2013/person.py
@@ -0,0 +1,434 @@
+#coding=utf8
+from uliweb.i18n import ugettext_lazy as _
+
+data = [
+
+    {
+	    'fullname':_('Lu Linqing'),
+		'nickname':_(''),
+		'company':_('Red Hat'),
+		'position':_('Quality Engineer'),
+		'desc':_('Lu Linqing , Lu is a Cloud Product Evangelist for OpenShift (PaaS by Red Hat) and has also been a Quality Engineer of Cloud/Virtualization Products in Red Hat for numerous years. Lu is also a Red Hat Certified Engineers (RHCE).'),
+		'avatar':'lulinqing.jpg',
+		'speech':_('《OpenShift - Develop and scale apps in the cloud 》'),
+	},
+
+
+    {
+	    'fullname':_('葛涵涛'),
+		'nickname':_(''),
+		'company':_('北京GDG(Beijing Google Developer Group)'),
+		'position':_('北京GDG 创始人'),
+		'desc':_('移动新观察(www.Mobiletmt.com)创办人，现在担任科力恒(北京)科技有限公司CEO，具有8年以上无线互联网行业经验及互联网互动应用类产品技术经验，曾担任3届微软Windows Phone MVP，也是微软.NET俱乐部的发起人和组织者。'),
+		'avatar':'bjgdg_ge.jpg',
+		'speech':_('《北京GDG介绍及GoogleIO大会新技术阅览》'),
+	},
+
+    {
+	    'fullname':_('程辉'),
+		'nickname':_(''),
+		'company':_('SINA'),
+		'position':_('新浪OpenStack公有云平台(SWS)负责人'),
+		'desc':_('新浪云计算技术经理，目前是OpenStack中国社区的管理员，同时也是新浪OpenStack公有云平台(SWS)负责人。2012年8月当选为OpenStack基金会董事会成员。'),
+		'avatar':'huicheng.jpg',
+		'speech':_('《基于OpenStack建设公有云平台的开发实践》'),
+	},
+
+    {
+	    'fullname':_('杜玉杰'),
+		'nickname':_('Ben'),
+		'company':_('九州云'),
+		'position':_(''),
+		'desc':_(''),
+		'avatar':'ben-duyujie.jpg',
+		'speech':_('《在中国如何开展OpenStack 社区》'),
+	},
+
+    {
+	    'fullname':_('陈峰'),
+		'nickname':_(''),
+		'company':_('SINA'),
+		'position':_('SAE Python开发工程师'),
+		'desc':_('介绍SAE Python云计算平台的现状，架构以及演变的历程。'),
+		'avatar':'sae_chenhui_h200.png',
+		'speech':_('《SAE Python的发展历程》'),
+	},
+
+    {
+	    'fullname':_('张沈鹏'),
+		'nickname':_('张教主'),
+		'company':_('42区'),
+		'position':_(''),
+		'desc':_('前豆瓣工程师,前美空工程师,42区创始人!'),
+		'avatar':'zsp.jpg',
+		'speech':_('《42qu.com 代码 & 架构 导读》'),
+	},
+
+
+	{
+	    'fullname':_('刘海丰'),
+		'nickname':_(''),
+		'company':_('盛大'),
+		'position':_('云引擎开发经理'),
+		'desc':_('刘海丰，2011年底加入盛大云计算公司，目前担任PaaS产品云引擎的开发经理。毕业于中国科技大学，之前曾任职于百度和雅虎北京研发中心。技术兴趣集中在分布式系统与云计算产品研发。'),
+		'avatar':'snda-liuhaifeng-200x200.jpg',
+		'speech':_('《盛大云引擎 – 发展中的PaaS平台》'),
+	},
+
+	{
+	    'fullname':_('孙毅'),
+		'nickname':_('Tachikoma'),
+		'company':_('豆瓣'),
+		'position':_('QA工程师'),
+		'desc':_('孙毅（Tachikoma），豆瓣QA工程师，负责豆瓣阅读的质量流程保证以及测试开发，持续集成等。'),
+		'avatar':'sunyi.jpg',
+		'speech':_('《豆瓣从开发到发布过程中的持续集成/发布工程实践》'),
+	},
+
+	{
+	    'fullname':_('童小军'),
+		'nickname':_(''),
+		'company':_('EasyHadoop'),
+		'position':_('创始人'),
+		'desc':_('前暴风影音在线研发部-搜索研发组主管'),
+		'avatar':'tongxiaojun.jpg',
+		'speech':_('《Python配合Hadoop 快速搭建开放型大数据仓库分析平台》'),
+	},
+
+    {
+	    'fullname':_('刘远亮'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('浙江大学本科毕业。美国密苏里大学哥伦比亚分校计算机科学硕士毕业，研究方向elearning。IEEE发表论文"Learning Objects的设计来支持Constructivism学习环境"。多年来致力于实现生活中的学习，改变现代教育与生活严重脱节的状况。09年辞掉在美国的工作，回到国内，进行知识引擎的开发。是知识引擎的设计者和开发者。'),
+		'avatar':'liuyuanliang.png',
+		'speech':_('《知识引擎的设计》'),
+	},
+
+	{
+	    'fullname':_('Zoom.Quiet'),
+		'nickname':_(''),
+		'company':_('金山网络'),
+		'position':_('过程改进经理'),
+		'desc':_('Python 中文社区创始人 / 管理员，热心于python社区的公益事业，大家熟知的社区"大妈"；OBP及蟒营工程设计者 /主持人；参与并主持各种线上 / 线下活动，主持编撰了《可爱的Python》；坚持用 Pythonic 感化国人进入 FLOSS世界进行学习 / 分享 / 创造！目前就职于金山软件。'),
+		'avatar':'zhouqi.jpg',
+		'speech':_('《如何 Pythonic 的组织技术交流》'),
+	},
+
+	{
+	    'fullname':_('李迎辉'),
+		'nickname':_('Limodou'),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('Python 中文社区创始人及 Python 技术的关键推广者之一。广受欢迎的 Ulipad 编辑器，以及 web框架 Uliweb 的作者。'),
+		'avatar':'liyinghui.jpg',
+		'speech':_(' 《又一年, UliWeb 发展纪事儿》'),
+	},
+
+	{
+	    'fullname':_('杨昆'),
+		'nickname':_(''),
+		'company':_('知乎'),
+		'position':_(''),
+		'desc':_('知乎服务器端 Tech Leader'),
+		'avatar':'yangkun.jpg',
+		'speech':_('《社会化问答网站中的Python应用》'),
+	},
+	
+	{
+	    'fullname':_('周辉'),
+		'nickname':_(''),
+		'company':_('华强方特影业投资有限公司'),
+		'position':_('技术研发部总监'),
+		'desc':_('周辉，华强方特影业投资有限公司，担任技术研发部总监一职。之前曾担任Base FX公司IT经理'),
+		'avatar':'zhouhui.jpg',
+		'speech':_('《视觉效果制作行业的工业语言——Python》'),
+	},
+
+	{
+	    'fullname':_('丛磊'),
+		'nickname':_(''),
+		'company':_('新浪'),
+		'position':_('新浪云计算首席架构师'),
+		'desc':_('丛磊，新浪云计算首席架构师，2006年毕业后加入新浪公司，2008年其带领技术团队从事云计算方向的技术研发，2009起先后经历了SAE alpha1、alpha2、beta的发布，目前负责SAE的整体技术架构和研发。'),
+		'avatar':'conglei.jpg',
+		'speech':_('《SAE 对Python 的深入支持及应用》'),
+	},
+
+	{
+	    'fullname':_('陆丹峰'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_('AdMaster研发副总裁'),
+		'desc':_('陆丹峰，AdMaster研发副总裁，主要负责领导公司产品研发和数据分析。组建了并负责管理北京研发团队，同时负责广告、舆情等公司核心产品线的架构与研发。曾于2005年联合创办英创思（Introns Tech.），主管产品研发，专注于大型网站质量诊断。2008 年加入日本电通广告在中国的子公司电众数码（c.），组建并主管 IT 部门，负责研发数字化媒体选择工具。陆丹峰毕业于北京大学，拥有应用数学硕士学位，曾以第一作者身份发表图像处理方向的 SCI 文章并由 Springer 收录，核心算法被多次引用。'),
+		'avatar':'ludanfeng.png',
+		'speech':_('《Python 在互联网广告监测分析中的应用》'),
+	},
+
+	{
+	    'fullname':_('董洵'),
+		'nickname':_(' Alex Dong'),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('对角巷创始人, 好看簿创始人之一. 微软2004年评出的全球56个架构师MVP中唯一的中国人. 他对历史和艺术有浓厚的兴趣, 相信结构的力量. 在不工作的时候, 他喜欢品南方绿茶,听各式音乐,看历史/经济/科幻/技术书籍. 曾经是半职业游泳运动员的他更喜欢绕着玉渊潭跑上15公里,出一身汗. 他还喜欢打篮球,中投自认还不赖.'),
+		'avatar':'',
+		'speech':_('《国外 Py 社区的组织以及技术发展特点》'),
+	},
+
+	{
+	    'fullname':_('曾庆生'),
+		'nickname':_(''),
+		'company':_('3Gland '),
+		'position':_(''),
+		'desc':_('2006年加入三技科技, 从事网络相关工作, 个人长期研究大陆互联网结构, 及解决大陆复杂网络环境中的快速网络访问、多线路相关方案, 为创业型公司提供个性化高性价比的解决方案.'),
+		'avatar':'zenqinsheng-200px.jpg',
+		'speech':_('《灵活的IDC业务中如何进行Python应用部属》'),
+	},
+
+	{
+	    'fullname':_('骆逸'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('@骆逸, 从事搜索引擎和推荐系统的研发，自2004年入Python教。现为毕肯互动（北京）科技有限公司总裁，之前曾任WPP ITOP 24/7公司CTO、空中网悟空搜索副总裁和美国Yahoo搜索资深工程师。毕业于清华大学和美国亚利桑那大学。'),
+		'avatar':'luoyi.jpg',
+		'speech':_('《廉价硬件和Python构建远程交流机器人开发计划》'),
+	},
+
+	{
+	    'fullname':_('王捷'),
+		'nickname':_(''),
+		'company':_('扇贝网'),
+		'position':_('扇贝网总经理'),
+		'desc':_('王捷，扇贝网总经理，毕业于东南大学和比利时鲁汶大学，曾先后在趋势科技中国研发中心，比利时Attentio，上海CIC担任开发工程师和研发总监等职位。'),
+		'avatar':'wangjie.jpg',
+		'speech':_('《Python小团队不妨知道的技术》'),
+	},
+
+	{
+	    'fullname':_('陈世欣'),
+		'nickname':_('Sting Chen'),
+		'company':_('2Style4You'),
+		'position':_('CTO'),
+		'desc':_('TopGeek社区创办人，现在全球领先的时尚科技公司2Style4You.com任CTO,1999年硕士毕业于上海交通大学计算机系，具有13年的互联网技术和互联网产品技术经验，还发起PMCamp社区等。也是ThinkinLAMP，ShanghaiOnRails，CPyUG，GTUG等社区的参与者或组织者。'),
+		'avatar':'chenshixin.jpg',
+		'speech':_('大会致辞《Python世界》'),
+	},
+
+	{
+	    'fullname':_('沈崴'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('资深 python 开发者，高效能服务器 eurasia 的作者。湖州迅普信息技术有限公司创始人，广州遛宝计算机技术有限公司联合创始人、技术总监。'),
+		'avatar':'shenwai.jpg',
+		'speech':_('《游戏逻辑编写技巧》《Python 产品构建与发布指南》'),
+	},
+
+	{
+	    'fullname':_('林伟'),
+		'nickname':_(''),
+		'company':_('网易'),
+		'position':_('资深技术主管'),
+		'desc':_('网易资深技术主管，18年程序设计经验。'),
+		'avatar':'linwei.jpg',
+		'speech':_('《实战游戏客户端》'),
+	},
+
+    
+
+	{
+	    'fullname':_('潘俊勇'),
+		'nickname':_(''),
+		'company':_('润普科技'),
+		'position':_('创始人，CTO'),
+		'desc':_('国内python社区最早的布道者之一，2002年开始创办Zope中文社区，同时也是润普公司创始人。2007年转向互联网软件，推出易度云办公(everydo.com)，专注企业云端办公方案。'),
+		'avatar':'panjunyong.jpg',
+		'speech':_('《演进或革命 - 选型和架构的实践经验谈》'),
+	},
+
+	{
+	    'fullname':_('黄冬'),
+		'nickname':_(''),
+		'company':_('土豆网'),
+		'position':_('技术副总裁'),
+		'desc':_('黄冬现任土豆网技术副总裁，主持设计和运营过多个大型高容量产品和系统。也是中国FreeBSD、Python社区的发起者和积极参与者，啄木鸟社区的创始人之一。此外，他还积极帮助各种创业项目，创立了彩虹流量和xBaynet系列产品。'),
+		'avatar':'huangdong.jpg',
+		'speech':_('《Python@土豆 （主题待定）》'),
+	},
+
+	{
+	    'fullname':_('赖勇浩'),
+		'nickname':_(''),
+		'company':_('广州银汉科技有限公司'),
+		'position':_('技术总监'),
+		'desc':_('网游行业资深从业者，有丰富的 python 游戏开发经验，也是珠三角技术沙龙的创办人和组委之一。'),
+		'avatar':'laiyonghao.jpg',
+		'speech':_('《网游开发中的 Python 组件》'),
+	},
+
+	{
+	    'fullname':_('王健'),
+		'nickname':_(''),
+		'company':_('广州黑崖网络科技有限公司'),
+		'position':_('总经理兼技术总监'),
+		'desc':_('广州黑崖网络科技有限公司总经理兼技术总监。网游行业资深从业者，代表作品有《富甲西游》，《喂哇游3D社区》，《月光之城online》等。'),
+		'avatar':'wangjian.jpg',
+		'speech':_('《自动化即时战斗游戏解决方案》'),
+	},
+
+	{
+	    'fullname':_('王剑峰'),
+		'nickname':_(''),
+		'company':_('OpenERP'),
+		'position':_(''),
+		'desc':_('2005年接触开源ERP，对各个开源ERP产品均有涉猎。主持参与了weberp、openbravo、openerp的汉化工作。2007年开始做OpenERP（原TinyERP）的本地化和推广。注重基于开源+威客的商业服务模式即实现程序员参与开源的长短期收益。现集中精力做OpenERP的远程二次开发团队，Python做ERP二次开发很爽，乐在其中......是http://openerp-china.org/ 站长和http://code.google.com/p/cloudteam/ 项目经理 微博：http://weibo.com/opennewbazaar 爱好：ERP，开源，威客'),
+		'avatar':'wangjianfeng.jpg',
+		'speech':_('《人人可以化半小时开发一个全功能OpenERP模块》'),
+	},
+	
+	{
+	    'fullname':_('许智翔'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('许智翔(Shell909090)，现任杭州齐治上海分公司程序员，python/c程序员，有7年工作经验，擅长系统/安全，主要活跃在shlug和cpug社区。他曾主持过多个大型系统的设计和开发，是python-segment和uniproxy的作者，也是python-formalchemy和python-snappy的debian包维护者。他喜欢太极，篮球和潜水，有兴趣的可以通过邮件或email联系他。'),
+		'avatar':'xuzhixiang.jpg',
+		'speech':_('《元编程在redis orm中的应用》'),
+	},
+
+	{
+	    'fullname':_('黄峥峼'),
+		'nickname':_('Mark Huang'),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('黄峥峼（Mark Huang）来自新加坡，现在在杭州一间小德国企业工作。 他已经code python四年了。 前一份工作在Lucasfilm Entertainment Company （卢卡斯电影有限公司）用Python写内部的Web Application。从Pylons Web 框架学起。他说：“我特别喜欢Python； 恨死Java。”'),
+		'avatar':'markhuang.jpg',
+		'speech':_('《Pyramid Web Framework for Advanced Web Applications》'),
+	},
+
+	{
+	    'fullname':_('李飞林'),
+		'nickname':_('飞龙非龙'),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('飞龙非龙（李飞林），Python技术博客 http://feilong.me 博主，Tornado 开源站点 http://poweredsites.org 作者，国内Tornado传教士。'),
+		'avatar':'feilong.jpg',
+		'speech':_('《用Tornado开发RESTful API应用》'),
+	},
+
+	{
+	    'fullname':_('董诣'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_(''),
+		'desc':_(' 董诣，曾就职于五分钟和盛大，正在开发3D flash页游&lt;深渊&gt;，负责开发游戏服务器端功能以及建立研发体系等工作。微博：http://weibo.com/dongyi1110 '),
+		'avatar':'dongyi.jpg',
+		'speech':_('《网页游戏的跨界开发》'),
+	},
+
+	
+	{
+	    'fullname':_('于夏桥'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('于夏桥，来自浙江农林大学网络中心'),
+		'avatar':'yuxiaqiao.jpg',
+		'speech':_('《使用 Python 和 Django 建设维护学校门户》'),
+	},
+
+	{
+	    'fullname':_('吴晗'),
+		'nickname':_(''),
+		'company':_(''),
+		'position':_(''),
+		'desc':_('吴晗，系统工程师，目前在淘宝做应用运维方面的工作，在linux系统运维方面有比较丰富的经验'),
+		'avatar':'wuhan.jpg',
+		'speech':_('《python在linux运维中的应用》'),
+	},
+    
+	{
+	    'fullname':_('Eric Caudal'),
+		'nickname':_(''),
+		'company':_('上海寰享网络科技公司'),
+		'position':_('CEO, OpenERP认证培训工程师, OpenERP高级项目经理'),
+		'desc':_('Eric Caudal  (法籍)上海寰享网络科技公司CEO, OpenERP认证培训工程师, OpenERP高级项目经理。他在信息系统项目管理方面有着10年以上的工作经验。Caudal先生在世界顶级食品公司McCain(炸薯条产业的世界主导者)工作12年,又在法国Panavi,欧洲顶级冷冻面包店担任规划署署长。从Mc-Cain 到Panavi, 他一直参与企业软件项目如ERP, 预先规划系统,客户关系管理系统等工作,在供应链和信息技术项目管理上获得了大量宝贵的业务经验。作为企业的管理者和/信息项目经理,Caudal先生在项目的实施中积极参与项目范围界定,信息服务跟进和关键决策制定。这些都归功于他有着和管理经验相结合,包括编程,网络或业务数据库等信息技术方面的坚实背景 。'),
+		'avatar':'eric_caudal.jpg',
+		'speech':_('《OpenERP即将推出的第7版的功能和新的编程框架介绍》'),
+	},
+
+    {
+        'fullname':_('刘飞'),
+    	'nickname':_(''),
+    	'company':_('阿里云'),
+    	'position':_('计算业务架构师'),
+    	'desc':_('刘飞，阿里云计算业务架构师，之前从事网格计算，分布式通信计算平台以及协同计算平台的研发工作。'),
+    	'avatar':'liufei.jpg',
+    	'speech':_('《阿里云之移动开发者上云》'),
+    },
+    {
+        'fullname':_('黄昶'),
+    	'nickname':_(''),
+    	'company':_('嬉相记网'),
+    	'position':_('创始人'),
+    	'desc':_('黄昶，福州人，嬉相记网站创始人，于2004年获得浙江大学计算机系硕士学位后加入IBM中国开发实验室，曾经领导超过五十人的产品研发团队，并拥有多项 专利技术。2008年他获得IBM孵化器的资助，通过企业内部创业，建立了IBM中国用户体验中心，并领导了如天涯社区、苏宁易购、中国银行网银等重大网 站用户体验重构项目。2011年，为了追逐创业梦想，黄昶离开IBM,进入INSEAD商学院深造，同年获得MBA学位。毕业后，他放弃了高薪职位，投身创办熟人兴趣社交网站 - 嬉相记。'),
+    	'avatar':'alan_huang.jpg',
+    	'speech':_('《NOSQL和MySQL在django上的混合使用》'),
+    },
+    {
+        'fullname':_('李小红'),
+    	'nickname':_(''),
+    	'company':_('土豆网'),
+    	'position':_('系统运营高级总监'),
+    	'desc':_(''),
+    	'avatar':'nopic.gif',
+    	'speech':_('《让程序运行更快》'),
+    },
+    {
+        'fullname':_('Tony Gu'),
+    	'nickname':_(''),
+    	'company':_('上海先安信息科技有限公司'),
+    	'position':_('联合创始人，OpenERP项目经理，前微软Dynamics AX ERP高级顾问'),
+    	'desc':_('Tony Gu - 上海先安信息科技有限公司联合创始人，OpenERP项目经理，前微软Dynamics AX ERP高级顾问。自然和科技的挚爱者，开源哲学的支持者。于2006年创建OpenERP/TinyERP中文开源社区：www.shine-it.net 。 积极组织和推动OpenERP本地化项目和社区组织建设。 2011年底创建先安科技并加入OpenERP官方合作伙伴，提供OpenERP相关专业服务。'),
+    	'avatar':'tony_gu.jpg',
+    	'speech':_('《人人可以化半小时开发一个全功能 OpenERP 模块》'),
+    },
+    {
+        'fullname':_('车蕙怡'),
+    	'nickname':_(''),
+    	'company':_('玩客网'),
+    	'position':_('市场总监'),
+    	'desc':_('资深游戏人，知名游戏 Treasure Land 制作者，超过八年Facebook应用程序制作经验，对海外市场有独到的见解，成功为玩客开拓国际市场'),
+    	'avatar':'chehuiyi.jpg',
+    	'speech':_('《Python如何帮助「逆转三国」获得成功》'),
+    },
+    {
+        'fullname':_('王力'),
+    	'nickname':_(''),
+    	'company':_('阿里云'),
+    	'position':_('高级专家、布道师'),
+    	'desc':_('从事基于阿里云的云计算解决方案的架构工作，并着力于阿里云开发者社区的建立与繁荣。加入阿里云之前在IBM CDL及CSTL担任资深软件工程师。2005年博士毕业于西安交大计算机系。您可通过weibo：王立-阿里云 与他联系。'),
+    	'avatar':'wangli.png',
+    	'speech':_('《飞天与阿里云云计算产品及服务》'),
+    },
+    {
+        'fullname':_('陈钢'),
+    	'nickname':_(''),
+    	'company':_('诺基亚西门子通信技术（北京）有限公司'),
+    	'position':_('软件工程师'),
+    	'desc':_('陈钢，软件工程师。2003年毕业于南京邮电学院。目前在诺基亚西门子通信技术（北京）有限公司，从事交换机设备类的平台软件开发和测试工作。主要的技术背景是嵌入式编程和C语言。在产品测试过程中对Python产生了浓厚的兴趣，对Python的实现机制有一定的研究。'),
+    	'avatar':'chengang.jpg',
+    	'speech':_('《用myTestCase模块来做python的UT》'),
+    },
+    {
+        'fullname':_('刘明华'),
+    	'nickname':_(''),
+    	'company':_('上海嘉值云计算科技'),
+    	'position':_('python程序员'),
+    	'desc':_('关注python、Unix/Linux、后端技术，关注云计算。开发过基于libvirt的虚拟机性能监控，并集成到Cloudopen上。热爱开源，热爱python。'),
+    	'avatar':'liuminghua.jpg',
+    	'speech':_('《基于nova的自动扩展和负载均衡》'),
+    },
+    
+]
