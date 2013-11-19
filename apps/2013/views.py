cfefbf03b8aa3c886fb7c7469f68a412d408761d
Eric Lo <lxneng@gmail.com>
2013-11-19 16:56:04 +0800

    rename routes

--- apps/2013/views.py
+++ apps/2013/views.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 from uliweb import expose
+import json
 
 
 pagetitle = "PyCon China 2013"
@@ -9,37 +10,43 @@ pagetitle = "PyCon China 2013"
 class SiteView2013(object):
     @expose('')
     def index(self):
-        return {"year":2013, "title":pagetitle}
-    
+        return {"year": 2013, "title": pagetitle}
+
     def about(self):
-        return dict(page=dict(pagename='about', cndata=''),title=pagetitle)
+        return dict(page=dict(pagename='about', cndata=''), title=pagetitle)
 
-    def schedulebj(self):
-        return dict(page=dict(pagename='schedulebj', cndata=''),title=pagetitle, year=2012)
+    def beijing(self):
+        return dict(page=dict(pagename='beijing', cndata=''),
+                    title=pagetitle, year=2013)
 
-    def schedulezh(self):
-        return dict(page=dict(pagename='schedulezh', cndata=''),title=pagetitle, year=2012)
+    def zhuhai(self):
+        return dict(page=dict(pagename='zhuhai', cndata=''),
+                    title=pagetitle, year=2013)
 
-    def scheduleca(self):
-        return dict(page=dict(pagename='scheduleca', cndata=''),title=pagetitle, year=2012)
+    def xi_an(self):
+        return dict(page=dict(pagename='xi_an', cndata=''),
+                    title=pagetitle, year=2013)
 
-    def schedulesh(self):
-        return dict(page=dict(pagename='schedulesh', cndata=''),title=pagetitle, year=2012)
+    def shanghai(self):
+        return dict(page=dict(pagename='shanghai', cndata=''),
+                    title=pagetitle, year=2013)
 
     def collections(self):
-        return dict(page=dict(pagename='collections', cndata=''),title=pagetitle)
+        return dict(page=dict(pagename='collections', cndata=''),
+                    title=pagetitle)
 
     def registration(self):
-        return dict(page=dict(pagename='registration', cndata=''),title=pagetitle)
+        return dict(page=dict(pagename='registration', cndata=''),
+                    title=pagetitle)
 
     def sponsors(self):
-        return dict(page=dict(pagename='sponsors', cndata=''),title=pagetitle)
+        return dict(page=dict(pagename='sponsors', cndata=''),
+                    title=pagetitle)
 
     def volunteer(self):
-        return dict(page=dict(pagename='volunteer', cndata=''),title=pagetitle)
+        return dict(page=dict(pagename='volunteer', cndata=''),
+                    title=pagetitle)
 
     def speakers(self):
         from person import data
         return json(data)
-
-
